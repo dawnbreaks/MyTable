54332935be598663a6ad63294fc568b1cc33c6c7
lubinwang <308181687@qq.com>
2014-06-25 14:50:27 +0800

    udpate readme.md

--- src/main/java/com/lubin/myTable/test/SimpleTest.java
+++ src/main/java/com/lubin/myTable/test/SimpleTest.java
@@ -13,22 +13,21 @@ public class SimpleTest {
 	public static void main(String[] args) throws UnsupportedEncodingException{
 		
 		 IMyTable client = MyTableClient.getInstance();
-		 String key = "hello world";
-		 byte[] value = bytes("leveldb");
-		 client.put(key, value);
-		 value = client.get(key);
-		 System.out.println(new String(value, "UTF-8"));
+		 String key = "hello";
+		 client.put(key,  bytes("myTable"));
+		 byte[] value = client.get(key);
+		 System.out.println("key="+key+"	value=" + new String(value, "UTF-8")+"\n");
 		 client.delete(key);
 		 
 		 
-		 client.put("a", bytes("leveldb"+ "a"));
-		 client.put("b", bytes("leveldb"+ "b"));
-		 client.put("c", bytes("leveldb"+ "c"));
-		 client.put("d", bytes("leveldb"+ "d"));
-		 client.put("e", bytes("leveldb"+ "e"));
+		 client.put("a", bytes("myTable:"+ "a"));
+		 client.put("b", bytes("myTable:"+ "b"));
+		 client.put("c", bytes("myTable:"+ "c"));
+		 client.put("d", bytes("myTable:"+ "d"));
+		 client.put("e", bytes("myTable:"+ "e"));
 		 Map<String, byte[]> res = client.range("a", "d");
 		 for(Map.Entry<String, byte[]> entry :res.entrySet()){
-			 System.out.println("key="+entry.getKey()+"|value="+new String(entry.getValue(), "UTF-8"));
+			 System.out.println("key="+entry.getKey() + "	value="+new String(entry.getValue(), "UTF-8"));
 		 }
 	}
 }
